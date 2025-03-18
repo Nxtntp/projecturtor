91999db26ede5a23efa7e809cff2daff99c1a3bc
Nxtntp <notnatapoom2545@gmail.com>
2025-03-18 23:14:52 +0700

    Create transporter.js

---
+++ utils/transporter.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+
+const AUTH_EMAIL = "netzanadonaja@gmail.com"
+const AUTH_PASS = "njyg rkds swdi suob"
+
+// const transporter = nodemailer.createTransport({
+//     service: "Gmail",
+//     secure: true,
+//     auth: {
+//         user: AUTH_EMAIL,
+//         pass: AUTH_PASS,
+//     },
+// });
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    service: "gmail",
+    auth: {
+        user: AUTH_EMAIL,
+        pass: AUTH_PASS,
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("Transporter error:", error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
+
+module.exports = transporter;
